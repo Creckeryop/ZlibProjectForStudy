bff093186ad54c9b36dae3778916dfe00101c816
Linkovschik <Linkovschik@users.noreply.github.com>
2020-07-24 01:03:49 +0500

    several changes

--- parsers/zlibrary.py
+++ parsers/zlibrary.py
@@ -111,46 +111,51 @@ def downloadBook(book, folder_path=""):
     global proxy, downloaded, bar
     counter = 0
     file_name = ""
-    for p in proxy:
-        print('Установка прокси: %s' % p)
-        setupProxy(p)
-        try:
-            print("Получение ссылки на загрузку")
-            url = getSoup(book['Link']).find(
-                'a', class_="addDownloadedBook")['href']
-            print("Загрузка файла по ссылке ("+sites['books'] + url+"). Размер: %s" % book['Size'])
-            downloaded = 0
-            bar = None
-            file_name = folder_path + ("[{0}] {1}.{2}".format(book['Author'], book['Name'], book["Format"])).replace(':', '').replace(
-                '*', '').replace('?', '').replace('/', '').replace('\\', '').replace('"', '').replace('<', '').replace('>', '').replace('|', '')
-            urllib.request.urlretrieve(
-                sites['books'] + url, file_name, progress_bar)
-            delete_last_line()
-            print("Файл загружен! [%s]" % file_name)
-            if checkHtml(file_name):
+    end_prog = True
+    while True:
+        for p in proxy:
+            print('Установка прокси: %s' % p)
+            setupProxy(p)
+            try:
+                print("Получение ссылки на загрузку")
+                url = getSoup(book['Link']).find(
+                    'a', class_="addDownloadedBook")['href']
+                print("Загрузка файла по ссылке ("+sites['books'] + url+"). Размер: %s" % book['Size'])
+                downloaded = 0
+                bar = None
+                file_name = folder_path + ("[{0}] {1}.{2}".format(book['Author'], book['Name'], book["Format"])).replace(':', '').replace(
+                    '*', '').replace('?', '').replace('/', '').replace('\\', '').replace('"', '').replace('<', '').replace('>', '').replace('|', '')
+                urllib.request.urlretrieve(
+                    sites['books'] + url, file_name, progress_bar)
+                delete_last_line()
+                print("Файл загружен! [%s]" % file_name)
+                if checkHtml(file_name):
+                    counter = counter + 1
+                    print("Прокси достигло лимита. Повтор")
+                    continue
+                else:
+                    break
+            except:
+                print("Прокси не подходит. Повтор")
                 counter = counter + 1
-                print("Прокси достигло лимита. Повтор")
                 continue
-            else:
-                break
-        except:
-            print("Прокси не подходит. Повтор")
-            counter = counter + 1
-            continue
-    if counter < len(proxy):
-        proxy = proxy[counter:]
-    else:
-        proxy = []
-    if len(proxy) == 0:
-        print("Прокси закончилось!")
-        while True:
-            print("Загрузите новое прокси в proxy.txt файл [Y - чтобы продолжить / n - закончить работу программы]")
-            s = input()
-            if s == 'N' or s == 'n' or s == "Y" or s == "y":
-                if s == 'Y' or s=='y':
-                    loadProxy()
-                break
-        
+        if counter < len(proxy):
+            proxy = proxy[counter:]
+        else:
+            proxy = []
+        if len(proxy) == 0:
+            print("Прокси закончилось!")
+            while True:
+                print("Загрузите новое прокси в proxy.txt файл [Y - чтобы продолжить / n - закончить работу программы]")
+                s = input()
+                if s == 'N' or s == 'n' or s == "Y" or s == "y":
+                    if s == 'Y' or s=='y':
+                        loadProxy()
+                        end_prog = False
+                    break
+        if end_prog:
+            break
+            
     return file_name
 
 def checkProxy():
