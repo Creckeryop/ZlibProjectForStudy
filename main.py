3f8b37cd1e1007d056bff29c3f65deda3d31667c
Creckeryop <didager@yandex.ru>
2020-08-17 19:12:58 +0500

    Мелкие штрижки + Добавлена база данных

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import googleDriveClass as Drive
 
 # tags = ['Киберфизические системы', 'Цифровая экономика', 'Индустрия 4.0', 'Умные системы', 'Интернет людей', 'Интернет вещей', 'Интернет сервисов', 'Архитектура киберфизической системы', 'Разнородность данных в киберфизической системе',
 #        'Надежность в киберфизической системе', 'Управление данными в киберфизической системе', 'Конфиденциальность в киберфизической системе', 'Безопасность в киберфизической системе', 'Реальное время в киберфизической системе']
-tags = ['Киберфизические системы', 'Цифровая экономика',
-        'Индустрия 4.0', 'Умные системы', 'Интернет людей']
+tags = ['Киберфизические системы', 'Цифровая экономика','Индустрия 4.0', 'Умные системы', 'Интернет людей']
 # подключаемся к базе данных
 cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
                       "Server=(LocalDb)\\MSSQLLocalDB;"
@@ -36,35 +35,43 @@ for tag in tags:
     print("Тэг '%s'" % tag)
     library = Zlib.getByTag(tag)
     library.extend(Zlib.getByTag(tag, "articles"))
+    print("Найдено: `"+str(len(library))+"` результатов по данному тэгу")
+    counter = 0
     for item in library:
+        if (len(str(item['Author']))>100):
+            continue
         print("%s - Автор: %s" % (item['Name'], item['Author']))
         cursor.execute("if exists (Select * from book where book.Download_Link=N'"+str(item['Link']).replace("'", "")+"') " +
-                       "select * from book")
-        try:
-            object_none = cursor.fetchone()[0]
-        except:
-            # Возможно нужно прежде чем скачивать, проверить на наличие этой книги в БД
-            # Здесь уже нужно сделать загрузку файла который скачался
-            # file_name путь к скаченной книге на диске
-            file_name = Zlib.downloadBook(item)
-            drive_url = uploader.Upload(FILENAME=file_name)
-            cursor.execute("if not exists (Select * from KeyWord where KeyWord.Name = N'" +
-                           tag+"') Insert into KeyWord (Name) values (N'"+tag+"')")
-            cnxn.commit()
-            cursor.execute("Insert into Book (Download_Link,Cloud_Link,Author,Name,Language,Format) " +
-                           "values (N'"+str(item['Link']).replace("'", "")+"',"+"N'"+str(drive_url).replace("'", "")+"',N'"+str(item['Author']).replace("'", "")+"',N'"+str(item['Name']).replace("'", "")+"','"+str(item['Language']).replace("'", "")+"','"+str(item['Format']).replace("'", "")+"')")
+                    "select * from book")
+        if counter < 5:
+            try:
+                object_none = cursor.fetchone()[0]
+            except:
+                    # Возможно нужно прежде чем скачивать, проверить на наличие этой книги в БД
+                    # Здесь уже нужно сделать загрузку файла который скачался
+                    # file_name путь к скаченной книге на диске
+                    file_name = Zlib.downloadBook(item)
+                    drive_url = uploader.Upload(FILENAME=file_name)
+                    cursor.execute("if not exists (Select * from KeyWord where KeyWord.Name = N'" +
+                                tag+"') Insert into KeyWord (Name) values (N'"+tag+"')")
+                    cnxn.commit()
+                    cursor.execute("Insert into Book (Download_Link,Cloud_Link,Author,Name,Language,Format) " +
+                                "values (N'"+str(item['Link']).replace("'", "")+"',"+"N'"+str(drive_url).replace("'", "")+"',N'"+str(item['Author']).replace("'", "")+"',N'"+str(item['Name']).replace("'", "")+"','"+str(item['Language']).replace("'", "")+"','"+str(item['Format']).replace("'", "")+"')")
+            counter += 1
         cursor.execute(
             "select book.Id from Book where book.Download_Link='"+str(item['Link'])+"'")
-        book_id = cursor.fetchone()[0]
-        print(book_id)
-        cursor.execute(
-            "select keyword.id from KeyWord where keyword.Name=N'"+tag+"'")
-        keyword_id = cursor.fetchone()[0]
-        cursor.execute("if not exists (select * from BookKeyWord where BookKeyWord.Book_Id = "+str(book_id)+" and BookKeyWord.KeyWord_Id = "+str(keyword_id)+") Insert into BookKeyWord (Book_Id, KeyWord_Id) values (" +
-                       str(book_id)+","+str(keyword_id)+")")
-        # сохраняем изменения в бд
-        cnxn.commit()
-
+        try:
+            book_id = cursor.fetchone()[0]
+            print(book_id)
+            cursor.execute(
+                "select keyword.id from KeyWord where keyword.Name=N'"+tag+"'")
+            keyword_id = cursor.fetchone()[0]
+            cursor.execute("if not exists (select * from BookKeyWord where BookKeyWord.Book_Id = "+str(book_id)+" and BookKeyWord.KeyWord_Id = "+str(keyword_id)+") Insert into BookKeyWord (Book_Id, KeyWord_Id) values (" +
+                        str(book_id)+","+str(keyword_id)+")")
+            # сохраняем изменения в бд
+            cnxn.commit()
+        except:
+            "lol"
         # Прокси закончилось, так что конец программы
         # Функция Zlib.downloadBook попросит дать ему новый прокси лист, если не хватит, так что не обращайте внимания на эти строки
         if not Zlib.checkProxy():
